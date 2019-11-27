d6d093a235a76c808cdbcb4a76a926b9e6f2c5b4
afontaneda <afontaneda@grupodetector.com>
2019-11-27 09:02:06 +0100

    Hotfix

--- app/src/main/java/com/gdetector/devicespermissions/MainActivity.java
+++ app/src/main/java/com/gdetector/devicespermissions/MainActivity.java
@@ -1,14 +1,7 @@
 package com.gdetector.devicespermissions;
 
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.gdetector.devicespermissionslibrary.DevicesPermissionActivity;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
